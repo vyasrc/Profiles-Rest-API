d71555f551dda457b9a47f56e9912bb8f392cea8
Vyas Cholayil <vyascholayil@gmail.com>
2019-12-22 20:02:30 -0500

    Added permissions and code for creating other users

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
 
 
 # Create your models here.
